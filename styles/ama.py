230b35f126fcec740a9916b001d0aef3aa7ec301
khanh <khanhtd@gmail.com>
2019-01-04 18:23:46 +0700

    Add dummy AMA style

---
+++ styles/ama.py
@@ -0,0 +1,15 @@
+# -*- coding: ascii -*-
+"""
+styles.ama
+~~~~~~~~~~
+
+Generate AMA style for article.
+"""
+
+__all__ = ['AMA']
+
+
+class AMA:
+    def __init__(self, **kwargs):
+        self.journal = ''
+        self.conference = ''
